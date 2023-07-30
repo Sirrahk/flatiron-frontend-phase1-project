782ec352e71f49bf78862b2c86cf387ce478ea99
Keara <keara.s.harris@gmail.com>
2023-07-30 03:46:54 -0500

    created eventlistener functions along with function names for the retrieval of object data

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 console.log("Hello World")
+
+
+//Event listener for monster button
+//Event listener for urban legends button
+//Event listener for demons button
+
+const monsterName = () => {
+    alert("yay you clicked me")
+    
+
+}
+
+const urbanLegends = ()=> {
+    alert("yay you clicked me")
+    
+
+}
+
+const demonsName = () => {
+    alert("yay you clicked me")
+}
+
+
+const displayMonster = () => {
+    monsterName();
+    urbanLegends();
+    demonsName();
+    //Will display monster on screen
+};
+
+const monsterEvent=() => {
+    const input = document.getElementsByClassName('.monsters')
+    input.addEventListener('click', displayMonster)
+}
+
+const demonsEvent = () => {
+    const demonInput =  document.getElementsByClassName('.demons')
+    demonInput.addEventListener('click', demonsName)
+}
+
+const scrollScreen = () => { 
+
+}
+//Form submission
